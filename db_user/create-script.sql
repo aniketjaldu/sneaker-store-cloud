de0b95bf03df94a7744d874e8065145a64403ef6
Denis Le <dl.denisle@gmail.com>
2025-07-16 14:55:08 -0400

    Add create script for user_database

---
+++ db_user/create-script.sql
@@ -0,0 +1,8 @@
+/********************************************************
+* This script creates the database named user_database
+*********************************************************/
+
+DROP DATABASE IF EXISTS user_database;
+CREATE DATABASE user_database;
+USE user_database;
+
